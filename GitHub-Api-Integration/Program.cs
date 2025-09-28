ca9d633c6fe1589931a0bc0efa35e0844a78b1e3
price <price@PATRYK>
2025-09-28 07:45:12 +0200

    Add project files.

---
+++ GitHub-Api-Integration/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Type your github username");
+
+string? username = Console.ReadLine();
+string uri = String.Format("https://api.github.com/users/{0}/events", username);
+
+var client = new HttpClient();
+var response = client.GetAsync(uri).Result.EnsureSuccessStatusCode();
+var responseString = await response.Content.ReadAsStringAsync();
