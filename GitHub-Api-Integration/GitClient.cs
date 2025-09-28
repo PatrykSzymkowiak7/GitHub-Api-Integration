6c794d318091c309282acac4aa7638253b2ffa48
Patryk <patrykszymkowiak7@gmail.com>
2025-09-28 09:19:17 +0200

    New method GetEvents()

--- GitHub-Api-Integration/GitClient.cs
+++ GitHub-Api-Integration/GitClient.cs
@@ -6,30 +6,35 @@ namespace GitHub_Api_Integration
 {
     public class GitClient
     {
+        private string Uri { get; }
+
+        private HttpClient Client { get; }
+
         public GitClient(string userName)
         {
-            var client = new HttpClient();
-            client.Timeout = TimeSpan.FromSeconds(10);
-            client.DefaultRequestHeaders.UserAgent.ParseAdd("GitHubApiIntegration/1.0");
-            string uri = String.Format("https://api.github.com/users/{0}/events", userName);
+            Client = new HttpClient();
+            Client.Timeout = TimeSpan.FromSeconds(10);
+            Client.DefaultRequestHeaders.UserAgent.ParseAdd("GitHubApiIntegration/1.0");
+            Uri = String.Format("https://api.github.com/users/{0}/events", userName);
+        }
+
+        public List<GitHubEvent> GetEvents()
+        {
+            List<GitHubEvent> events = new List<GitHubEvent>();
             HttpResponseMessage response;
             try
             {
-                response = client.GetAsync(uri).Result;
+                response = Client.GetAsync(Uri).Result;
                 response.EnsureSuccessStatusCode();
                 string responseBody = response.Content.ReadAsStringAsync().Result;
-                var events = JsonSerializer.Deserialize<List<GitHubEvent>>(responseBody);
-                foreach(var eve in events)
-                {
-                    Console.WriteLine($"[{eve.CreatedAt}] {eve.Actor.Login} did {eve.Type} in repo {eve.Repo.Name}");
-                }
+                events = JsonSerializer.Deserialize<List<GitHubEvent>>(responseBody);
             }
-            catch(Exception ex)
-            { 
+            catch (Exception ex)
+            {
                 throw ex;
             }
-        }
 
-        
+            return events;
+        }
     }
 }
